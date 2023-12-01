5d6189a3d93bfd88fbc6d77df087fd8e03a7781d
Christian Donker <45289692+Christian199815@users.noreply.github.com>
2023-12-01 13:27:08 +0100

    html css meuk

---
+++ scripts/car-selection.js
@@ -0,0 +1,38 @@
+const carSelectionButton = document.querySelector("section:nth-of-type() a:first-of-type");
+
+var folgoreValues = [
+    '761HP',
+    '325 km/h',
+    '2.7 sec',
+    'AWD',
+    'none',
+    '1350 Nm'
+];
+
+var trofeoValues = [
+    '550HP',
+    '320 km/h',
+    '3.5 sec',
+    'AWD',
+    'V6',
+    '660 Nm'
+];
+
+var modenaValues = [
+    '490HP',
+    '302 km/h',
+    '3.9 sec',
+    'AWD',
+    'V6',
+    '600 Nm'
+];
+
+var modelNames = [
+    'FOLGORE',
+    'TROFEO',
+    'MODENA'
+];
+
+
+var currentCarSelectionIndex = 0;
+
--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,7 @@ console.log("hi Merle");
 
 const scrollline = document.querySelector('.scroll-line');
 const folgoreVideo = document.getElementById('folgoreVideo');
+const footerButton1 = document.querySelector('footer ul li button');
 
 
 var currentIndex = 0;
@@ -83,6 +84,22 @@ function checkVideo()
 
 window.addEventListener('scroll', checkVideo);
 
-
-
+footerButton1.onclick = DropdownToggle();
+function DropdownToggle() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
 
